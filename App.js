d8327d91f532d0e8c205e2092eb53984a20ef17d
Suraj <bissuraj@gmail.com>
2022-03-06 13:43:34 -0500

    Did stuff

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Text,
   Alert,
   Dimensions,
+  TouchableOpacity,
 } from "react-native";
 
 import {
@@ -24,18 +25,32 @@ const App = () => {
   ]);
 
   const [data2, setData2] = useState([1]);
+  const [shouldTick, setShouldTick] = useState(false);
+  const [value, setValue] = useState(1000);
 
   const incrementData = () => {
-    setData2((prev) => [...prev, prev.pop() * 1.05]);
+    setData2((prev) => {
+      if (shouldTick) {
+        if (prev.length > 10) {
+          const newPrev = prev.slice(1);
+          return [...newPrev, newPrev.pop() * 1.05];
+        } else {
+          return [...prev, prev.pop() * 1.05];
+        }
+      } else {
+        return prev;
+      }
+    });
   };
 
   useEffect(() => {
     const tick = setInterval(() => {
       incrementData();
+      //incrementData();
     }, 250);
 
     return () => clearInterval(tick);
-  }, []);
+  }, [shouldTick]);
 
   const graphStyles = StyleSheet.create({
     container: {
@@ -59,6 +74,18 @@ const App = () => {
     },
   });
 
+  const BUY_AMT = 100;
+
+  const handleBuy = () => {
+    setShouldTick(true);
+    setValue(value - BUY_AMT);
+  };
+
+  const handleSell = () => {
+    setShouldTick(false);
+    setValue(value + data2[data2.length - 1]);
+  };
+
   return (
     <SafeAreaView styles={graphStyles.container}>
       <Text>Crash v1</Text>
@@ -68,38 +95,70 @@ const App = () => {
           datasets: [
             {
               data: data2,
+              color: () => "#fff",
             },
           ],
         }}
         width={Dimensions.get("window").width} // from react-native
-        height={220}
-        yAxisLabel="$"
-        yAxisSuffix="k"
+        height={400}
+        yAxisLabel=""
+        yAxisSuffix=""
         yAxisInterval={1} // optional, defaults to 1
         chartConfig={{
-          backgroundColor: "#e26a00",
-          backgroundGradientFrom: "#fb8c00",
-          backgroundGradientTo: "#ffa726",
+          backgroundGradientFrom: shouldTick ? "#14b8a6" : "#16a34a",
+          backgroundGradientTo: shouldTick ? "#14b8a6" : "#16a34a",
           decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          color: (opacity = 2) => `rgba(255, 255, 255, ${opacity})`,
           labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
           style: {
             borderRadius: 16,
           },
+          strokeWidth: "7",
           propsForDots: {
             r: "6",
             strokeWidth: "2",
             stroke: "#ffa726",
           },
         }}
-        //bezier
+        withDots={false}
+        withShadow={false}
         style={{
           marginVertical: 8,
           borderRadius: 16,
         }}
       />
+      <View style={styles.buttonContainer}>
+        <Text>{Math.round(data2[data2.length - 1])}</Text>
+        <Text>{`$ ${value}`}</Text>
+
+        <TouchableOpacity
+          onPress={shouldTick ? () => handleSell() : () => handleBuy()}
+        >
+          <Text style={styles.buyButton}>{shouldTick ? "SELL" : "BUY"}</Text>
+        </TouchableOpacity>
+        {/* 
+        <TouchableOpacity onPress={() => handleBuy()}>
+          <Text style={styles.buyButton}>BUY</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleSell()}>
+          <Text style={styles.buyButton}>SELL</Text>
+        </TouchableOpacity> */}
+
+        <Button title="SELL"></Button>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  buyButton: {
+    color: "red",
+    fontSize: 18,
+    //display: shouldTick && "none",
+  },
+  buttonContainer: {
+    alignItems: "center",
+  },
+});
 
 export default App;
